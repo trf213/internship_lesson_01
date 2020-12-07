e02966119e364c58657996418389092634222b68
trf <trf.studios246@gmail.com>
2020-12-07 14:53:13 -0400

    feat: Update Login and redirect.

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -7,6 +7,14 @@ export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null)
     const loginUser = (data) => {
         //TODO: Handle login functionality here
+       
+            setCurrentUser({
+                'name': data.username,
+                'isLoggedIn': true,
+            });
+            window.localStorage.setItem('user', data.username);
+            window.location.replace("/todo");
+        
         //TODO: Store login info in localstorage (don't store password!)
     }
     return (
